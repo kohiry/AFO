2b6ca18f2fd77ccff02ee59a23d3c0e2898f783d
kohiry <kohiry@mail.ru>
2023-10-11 02:30:11 +0400

    fix: закомментировал и очистил ненужные импорты в main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from flask import Flask
 from config import DBConfig, Config
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DBConfig.get_db_url()
-app.config["SECRET_KEY"] = Config.get_secret_key()
+# app.config["SQLALCHEMY_DATABASE_URI"] = DBConfig.get_db_url()
+# app.config["SECRET_KEY"] = Config.get_secret_key()
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
