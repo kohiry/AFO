5a8633be3eb39cc67a4d29f4684d7f24a5607487
kohiry <kohiry@mail.ru>
2023-10-11 02:29:23 +0400

    fix: убрал нерабочие эндпоинты

--- app/router.py
+++ app/router.py
@@ -1,10 +1,11 @@
 from main import app
-from services import UserManager
-from schemas import UserSchema
-from typing import Optional
 
 
 @app.route("/")
 @app.route("/index")
 def hello_world():
     return "aboba"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
