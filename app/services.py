9f4ff14fd859f8b9085edb02d16318db888f279a
kohiry <kohiry@mail.ru>
2023-10-11 01:47:47 +0400

    feat: создал менеджер Юзера

--- app/services.py
+++ app/services.py
@@ -2,14 +2,26 @@ from models import User
 from schemas import UserSchema
 
 
+class UserManager:
+    """Класс для взаимодействия с пользователем."""
+
+    def get_by_id(user_id: str) -> UserSchema:
+        """Метод получения схемы юзера по его id."""
+        return User.check_user(user_id)
+
+
 class CastUserAndUserSchema:
+    """Преобразование типов Схемы юзера и Модели юзера в друг друга."""
+
     def __init__(self, user: User):
         self._user = user
 
-    def db_to_schema(self):
-        user_schema = UserSchema()
-        user_schema.email = self._user.email
-        user_schema.id = self._user.id
-        user_schema.password = self._user.password
-        user_schema.username = self._user.username
+    def db_to_schema(self) -> UserSchema:
+        """Преобразование модели в схему."""
+        user_schema = UserSchema(
+            email=self._user.email,
+            id=self._user.id,
+            password=self._user.password,
+            username=self._user.username,
+        )
         return user_schema
