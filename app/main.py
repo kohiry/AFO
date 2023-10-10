7bef54dfa17301d4d7372ec7a9cbab10ebdb4e0a
kohiry <kohiry@mail.ru>
2023-10-11 01:50:15 +0400

    fix: удаляю старые наработки из main.py

--- app/main.py
+++ app/main.py
@@ -3,19 +3,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import DBConfig, Config
-from flask_wtf.csrf import CSRFProtect
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DBConfig.get_db_url()
 app.config["SECRET_KEY"] = Config.get_secret_key()
 
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
-login_manager.login_message_category = "info"
-
-bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
-csrf.init_app(app)
