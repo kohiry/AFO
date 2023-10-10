61683579e25a24afb2a2c317d6a8d9f19dbb6e59
kohiry <kohiry@mail.ru>
2023-10-11 01:48:22 +0400

    feat: добавил uuid в модель юзера

--- app/models.py
+++ app/models.py
@@ -2,13 +2,15 @@ from main import db
 from flask_login import UserMixin
 from services import CastUserAndUserSchema as castU
 from schemas import UserSchema
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     def check_user(self, user_id: str) -> UserSchema:
-        return castU(self.query.get(int(user_id))).db_to_schema()
+        return castU(self.query.get(user_id)).db_to_schema()
