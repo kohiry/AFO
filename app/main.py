5c12ce05267f69613203819c38a1b33c87ecc0c6
kohiry <kohiry@mail.ru>
2023-10-11 00:30:27 +0400

    fix: удалил шаблоны из приложения фласк

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from config import DBConfig
+from flask import Flask, render_template
+from config import DBConfig, Config
+from flask_login import login_user, current_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, template_folder="/templates")
+
+app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DBConfig.get_db_url()
+app.config["SECRET_KEY"] = Config.get_secret_key()
 
 
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
