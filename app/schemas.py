610534ab1f26ab87089385505fb5ee231ffc1b9f
kohiry <kohiry@mail.ru>
2023-10-11 13:20:46 +0400

    feat: добавил схему банковских реквизитов

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel, Field, EmailStr
+from typing import List
 from uuid import UUID
+
+
+class BankSchema(BaseModel):
+    id: UUID
+    bank_name: str
+    kor_score: str
+    swift: str
+    iban: str
+    user_id: UUID
 
 
 class UserSchema(BaseModel):
@@ -7,3 +17,4 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr = Field(examples=["marcelo@mail.com"])
     password: str
+    banks: List[BankSchema]
