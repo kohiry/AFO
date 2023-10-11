438bb92360d9620130161f672ed13e76c2ef63e3
kohiry <kohiry@mail.ru>
2023-10-11 13:20:15 +0400

    feat: добавил магический метод repr для удобного просмотра объектов моделей и создание нового юзера

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ import uuid
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from schemas import UserSchema
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -17,6 +18,17 @@ class User(Base):
     password = Column(String(60), nullable=False)
     banks = relationship("BankReq", backref="user", lazy=True)
 
+    @staticmethod
+    def new(us: UserSchema):
+        new_user = User(
+            id=us.id, username=us.username, enail=us.email, password=us.password
+        )
+        Base.session.add(new_user)
+        Base.session.commit()
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
 
 class BankReq(Base):
     __tablename__ = "banks_req"
@@ -28,3 +40,6 @@ class BankReq(Base):
     swift = Column(String(11), nullable=False)
     iban = Column(String(34), nullable=True)
     user_id = Column(UUID, ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<Bank Req {self.bank_name} user_id {self.user_id}>"
