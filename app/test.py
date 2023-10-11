09482bd7681d185024f92394c6e29f2ede45a03f
kohiry <kohiry@mail.ru>
2023-10-11 13:48:56 +0400

    feat: создаю тесты для крудов

--- app/test.py
+++ app/test.py
@@ -1,13 +1,28 @@
+import pytest
 from schemas import UserSchema
 from services import UserCRUD
 import uuid
 
-UserCRUD.create(
-    UserSchema(
-        id=uuid.uuid4(),
+
+def test_user_creation():
+    # Генерируем уникальный UUID для пользователя
+    user_id = uuid.uuid4()
+
+    # Создаем объект UserSchema
+    user_data = UserSchema(
+        id=user_id,
         email="hi@mail.ru",
         username="hello",
         password="123",
         banks=[],
     )
-)
+
+    # Вызываем функцию создания пользователя и проверяем, что она не вызывает исключений
+    try:
+        UserCRUD.create(user_data)
+    except Exception as e:
+        pytest.fail(f"User creation failed: {e}")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
