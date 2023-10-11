3f2fb9f2676282ae43fa5ffc5cb15d1dd034e109
kohiry <kohiry@mail.ru>
2023-10-11 19:00:03 +0400

    feat: добавил логин менеджер и настройки для csrf

--- app/forms.py
+++ app/forms.py
@@ -1,24 +1,22 @@
-# например forms.py
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label="email", validators=[DataRequired()])
-    password = PasswordField(label="password", validators=[DataRequired()])
-
-
-class RegistrationForm(Form):
-    username = StringField("Имя пользователя", [validators.Length(min=4, max=25)])
-    email = StringField("Email-адрес", [validators.Length(min=6, max=35)])
-    password = PasswordField(
-        "Новый пароль",
-        [
-            validators.DataRequired(),
-            validators.EqualTo("confirm", message="Пароли должны совпадать"),
-        ],
-    )
-    confirm = PasswordField("Повторите пароль")
-    accept_tos = BooleanField("Я принимаю TOS", [validators.DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
