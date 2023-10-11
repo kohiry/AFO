0f80f57ddf99c6e838733464c5d29e1271d6476b
kohiry <kohiry@mail.ru>
2023-10-12 01:02:05 +0400

    feat: создал рабчую форму login

--- app/router.py
+++ app/router.py
@@ -1,4 +1,6 @@
 from main import app
+import schemas
+import services
 from flask import render_template, request, redirect, url_for, session
 
 
@@ -10,11 +12,18 @@ def home():
         return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        pwd = request.form["pwd"]
+        userform = schemas.LoginSchema(
+            username=request.form["username"], password=request.form["password"]
+        )
+        if services.UserCheck.check(userform):
+            session["username"] = userform.username
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", error="Invalid username or password")
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
